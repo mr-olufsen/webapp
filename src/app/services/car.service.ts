a3d941a38537a28a65a7514c9ed9120f79d17ba1
J.Olufsen <57617610+mr-olufsen@users.noreply.github.com>
2024-06-26 10:26:20 +0300

    add demo data

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import {Observable, of, throwError} from 'rxjs';
 import {map} from "rxjs/operators";
 import { User } from '../models/user.model';
 import { dev } from '../../environments/dev';
 import {HttpClient} from "@angular/common/http";
 import {Car} from "../models/car.model";
+
+import demoData from '../assets/demo-data.json';
 
 @Injectable({
   providedIn: 'root'
@@ -12,15 +14,30 @@ import {Car} from "../models/car.model";
 export class CarService {
   private baseUrl = `${dev.apiBaseUrl}/cars`;
 
+  private cars: Car[] = demoData.flatMap(user => user.cars);
+
   constructor(private http: HttpClient) { }
 
   getCars(): Observable<Car[]> {
-    return this.http.get<Car[]>(this.baseUrl);
+    return of(this.cars);
   }
 
-  getCarById(id : number): Observable<Car> {
-    return this.http.get<Car>(this.baseUrl + '/' + id);
-
+  getCarById(id: number): Observable<Car> {
+    const car = this.cars.find(car => car.id === id);
+    if (car) {
+      return of(car);
+    } else {
+      return throwError(() => new Error(`Car with id ${id} not found`));
+    }
   }
+
+  // getCars(): Observable<Car[]> {
+  //   return this.http.get<Car[]>(this.baseUrl);
+  // }
+  //
+  // getCarById(id : number): Observable<Car> {
+  //   return this.http.get<Car>(this.baseUrl + '/' + id);
+  //
+  // }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import {Observable, of, throwError} from 'rxjs';
 import {map} from "rxjs/operators";
 import { User } from '../models/user.model';
 import { dev } from '../../environments/dev';
 import {HttpClient} from "@angular/common/http";
+
+import demoData from '../assets/demo-data.json';
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,18 +14,35 @@ import {HttpClient} from "@angular/common/http";
 export class UserService {
   private baseUrl = `${dev.apiBaseUrl}/users`;
 
+  private users: User[] = demoData;
+
   constructor(private http: HttpClient) { }
 
+
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.baseUrl).pipe(
+    return of(this.users).pipe(
       map(users => this.addUserReferenceToCars(users))
     );
   }
 
-  getUserById(id : number): Observable<User> {
-    return this.http.get<User>(this.baseUrl + '/' + id);
-
+  getUserById(id: number): Observable<User> {
+    const user = this.users.find(user => user.id === id);
+    if (user) {
+      return of(user);
+    } else {
+      return throwError(() => new Error(`User with id ${id} not found`));
+    }
   }
+
+  // getUsers(): Observable<User[]> {
+  //   return this.http.get<User[]>(this.baseUrl).pipe(
+  //     map(users => this.addUserReferenceToCars(users))
+  //   );
+  // }
+  //
+  // getUserById(id : number): Observable<User> {
+  //   return this.http.get<User>(this.baseUrl + '/' + id);
+  // }
 
   private addUserReferenceToCars(users: User[]): User[] {
     const userMap = new Map<number, User>();
